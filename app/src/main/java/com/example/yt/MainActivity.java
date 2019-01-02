aed8e95cb6c59bd8f44dbacd18182b63fc370ba2
1711605019 <614754215@qq.com>
2019-01-02 11:48:01 +0800

    修改Maintivity1

--- app/src/main/java/com/example/yt/MainActivity.java
+++ app/src/main/java/com/example/yt/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.yt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn_student,btn_teacher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_student=findViewById(R.id.btn_main_student);
+        btn_teacher=findViewById(R.id.btn_main_teacher);
+
+        btn_student.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               stu();
+            }
+        });
+        btn_teacher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tea();
+            }
+        });
+    }
+    public void stu(){
+        Intent intent=new Intent(this,StudentActivity.class);
+        startActivity(intent);
+    }
+    public void tea(){
+        Intent intent=new Intent(this,StudentActivity.class);
+        startActivity(intent);
     }
 }
