7a12a1fe4d812ee4e3f58495b6e2288dbd266fad
1711605019 <614754215@qq.com>
2019-01-04 12:41:14 +0800

    项目修改12

--- app/src/main/java/com/example/yt/DeleteActivity.java
+++ app/src/main/java/com/example/yt/DeleteActivity.java
@@ -1,10 +1,13 @@
 package com.example.yt;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -54,6 +57,39 @@ public class DeleteActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+    }
+    public class deleteDialog extends Dialog {
+        Button btn_ok,btn_cancel;
+        String dialogName;
+        public deleteDialog(Context context, String dialogName){
+            super(context);
+            this.dialogName=dialogName;
+        }
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            requestWindowFeature(Window.FEATURE_NO_TITLE);
+            setContentView(R.layout.activity_delete_dialog);
+            btn_ok=findViewById(R.id.btn_dialog_ok);
+            btn_cancel=findViewById(R.id.btn_dialog_cancel);
+
+            btn_cancel.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dismiss();
+                }
+            });
+            btn_ok.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    MySQLiteAdapter adapter=new MySQLiteAdapter(getApplicationContext(),"database.db");
+                    adapter.deleteAll();
+                    dismiss();
+                }
+            });
+
+        }
 
     }
 }
