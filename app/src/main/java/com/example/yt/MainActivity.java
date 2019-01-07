72f6b9548f40782bd8ada229c6084091b53da5b5
1711605019 <614754215@qq.com>
2019-01-07 17:03:42 +0800

    项目修改24

--- app/src/main/java/com/example/yt/MainActivity.java
+++ app/src/main/java/com/example/yt/MainActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,11 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         btn_student=findViewById(R.id.btn_main_student);
         btn_teacher=findViewById(R.id.btn_main_teacher);
         btn_net=findViewById(R.id.btn_main_net);
-
         btn_student.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
